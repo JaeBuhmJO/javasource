07847e1a688aa377a0d2ae3a9ad258e1558f8817
Jaebuhm <favor99531@gmail.com>
2023-03-16 17:12:29 +0900

    Main modified

--- a_Prac/src/main/Main.java
+++ a_Prac/src/main/Main.java
@@ -1,33 +1,28 @@
 package main;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
-		// 문제에서 준 조건
 		try (Scanner scanner = new Scanner(System.in)) {
-			int t = Integer.parseInt(scanner.nextLine());
-			String input = scanner.nextLine();
-		
-		
-//		System.out.println(isPalindrome(input)+" "+함수 호출횟수);
-
-
+			int n = scanner.nextInt();
+			int numarr[] = new int[n];
+			
+			for (int i = 0; i < n; i++) {
+				int num = scanner.nextInt();
+				numarr[i] = num;
+			}
+			//시간 초과
+			Arrays.sort(numarr);
+            
+			for (int i : numarr) {
+			System.out.println(i);
+			}
+			
 		}
 
 	}
 
-	// 문제에서 준 메소드
-	public static int recursion(String s, int l, int r) {
-		if (l >= r)
-			return 1;
-		else if (s.charAt(l) != s.charAt(r))
-			return 0;
-		else
-			return recursion(s, l + 1, r - 1);
-	}
-
-	public static int isPalindrome(String s) {
-		return recursion(s, 0, s.length() - 1);
-	}
 }
