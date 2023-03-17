dd982aa4f9f8d9a4ce4adbf8f68ae2db6bd5ffe9
Jaebuhm <favor99531@gmail.com>
2023-03-17 11:01:42 +0900

    constructor created, toString Override

--- ch7/src/util/Test1.java
+++ ch7/src/util/Test1.java
@@ -6,11 +6,23 @@ public class Test1 {
 	private int no;
 	private String tel;
 
+	public Test1(String name, int no, String tel) {
+		super();
+		this.name = name;
+		this.no = no;
+		this.tel = tel;
+	}
+
 	public void print() {
 	}
 
 	public int sum() {
 		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return "Test1 [name=" + name + ", no=" + no + ", tel=" + tel + "]";
 	}
 
 }
