db4a85f638b89d9f05be0e7d98869a7b196065c8
Jaebuhm <favor99531@gmail.com>
2023-03-17 10:00:30 +0900

    Main 마지막 커밋

--- a_Prac/src/main/Main.java
+++ a_Prac/src/main/Main.java
@@ -4,26 +4,34 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-			int n = Integer.parseInt(br.readLine());
-			int numarr[] = new int[n];
+			Set<String> set = new HashSet<>();
+			
+			String input[] = br.readLine().split(" ");
+			int n = Integer.parseInt(input[0]);
+			int m = Integer.parseInt(input[1]);
+			
 			for (int i = 0; i < n; i++) {
-				int num = Integer.parseInt(br.readLine());
-				numarr[i] = num;
+				String word = br.readLine();
+				set.add(word);
 			}
-			Arrays.sort(numarr);
-
-			for (int i : numarr) {
-				bw.write(Integer.toString(i));
-				bw.newLine();
+			
+			int count = 0;
+			for (int i = 0; i < m; i++) {
+				String word = br.readLine();
+				if (set.contains(word)) {
+					count++;
+				}
 			}
-
+			bw.write(Integer.toString(count));
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
